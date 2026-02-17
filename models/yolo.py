bf1deb9141b279b059b9a045e09d41e2c08911bc
saly234 <maodeshuwu123@qq.com>
2026-02-17 17:59:40 +0800

    Add GCAM to the list of imports in yolo.py
    Signed-off-by: saly234 <maodeshuwu123@qq.com>

--- models/yolo.py
+++ models/yolo.py
@@ -421,6 +421,7 @@ def parse_model(d, ch):
             nn.ConvTranspose2d,
             DWConvTranspose2d,
             C3x,
+            GCAM,
         }:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
